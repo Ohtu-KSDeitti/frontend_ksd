d0b8a8fc47403b9afc71110a3bf83de6e1495720
k0psutin <mr.gravez@gmail.com>
2021-05-19 15:06:37 +0300

    GraphQL added

--- src/App.js
+++ src/App.js
@@ -5,22 +5,35 @@ import Menu from './components/Menu'
 import {
   Switch, Route
 } from "react-router-dom"
+import { useQuery, gql } from '@apollo/client'
 
-const App = () => (
-  <div className="container">
-    <h1>Kristittyjen sinkkujen deitti</h1>
-    <Menu />
-    <Switch>
-      <Route path= "/login">
-        <LoginForm
-        />      
-      </Route>
-      <Route path= "/register">
-        <RegistrationForm
-        />
-      </Route>
-    </Switch>
-  </div>
-)
+const GET_PING = gql`
+  query {
+    pings {
+      ping
+    }
+  }
+`
+
+const App = () => {
+  const result = useQuery(GET_PING)
+  console.log(result.data)
+  return (
+      <div className="container">
+        <h1>Kristittyjen sinkkujen deitti</h1>
+        <Menu />
+        <Switch>
+          <Route path= "/login">
+            <LoginForm
+            />      
+          </Route>
+          <Route path= "/register">
+            <RegistrationForm
+            />
+          </Route>
+        </Switch>
+      </div>
+  )
+}
 
 export default App
--- src/index.js
+++ src/index.js
@@ -10,9 +10,21 @@ import {
   BrowserRouter as Router,
 } from "react-router-dom"
 
+const ENV = process.REACT_APP_ENV || 'development'
+const URI = (ENV !== 'development') ? process.REACT_APP_BACKEND : 'http://localhost:4000'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: URI
+  })
+})
+
 ReactDOM.render(
+  <ApolloProvider client={client}>
   <Router>
     <App />
-  </Router>,
+  </Router>
+  </ApolloProvider>,
   document.getElementById('root')
 )
