738c6953108560c24f0b1f247da2b25fd0c5958b
StrappedGlint13 <matiasbrax@gmail.com>
2021-05-21 18:22:49 +0300

    dod update

--- documentation/definition_of_done.md
+++ documentation/definition_of_done.md
@@ -9,18 +9,20 @@ Viitekehys CDIO:
 - Suunniteltu
 - Ohjelmoitu
   - Kieli: JavaScript
-  - Frontend: React ja GraphQL
-  - Backend: Node.js, GraphQL ja Apollo
-  - Tietokanta: MongoDB  
+  - Front End: React ja GraphQL
+  - Back End: Node.js, GraphQL ja Apollo
+  - Tietokanta: Auki, mahdollisesti DynamoDB  
 - Testattu
-  - Koodin laatu tarkistettu ESlint:llä (Google versio)
-  - Front- & Backend: Jest:llä
+  - Testikattavuus vähintään 70%
+  - Koodin laatu tarkistettu ESlint:llä
+    - Front End: Airbnb
+    - Back End: Google 
+  - Front & Back End: Jest:llä
   - End-to-end: Cypress
 - Testaus automatisoitu
   - GitHub actions
-- Dokumentoitu ??
-  - LISÄÄ TÄHÄN GOOGLETUS
+- Dokumentoitu
 ### Implement:
 - Integroitu muuhun ohjelmistoon
 ### Operate:
-- Staging-palvelin: Heroku
+- Staging-palvelin: Auki, mahdollisesti AWS
--- src/App.js
+++ src/App.js
@@ -2,23 +2,36 @@ import React from 'react'
 import {
   Switch, Route,
 } from 'react-router-dom'
+import { useQuery, gql } from '@apollo/client'
 import LoginForm from './components/LoginForm'
 import RegistrationForm from './components/RegistrationForm'
 import Menu from './components/Menu'
 
-const App = () => (
-  <div className="container">
-    <h1>Kristittyjen sinkkujen deitti</h1>
-    <Menu />
-    <Switch>
-      <Route path="/login">
-        <LoginForm />
-      </Route>
-      <Route path="/register">
-        <RegistrationForm />
-      </Route>
-    </Switch>
-  </div>
-)
+const GET_PING = gql`
+  query {
+    pings {
+      ping
+    }
+  }
+`
+
+const App = () => {
+  const result = useQuery(GET_PING)
+  console.log(result)
+  return (
+    <div className="container">
+      <h1>Kristittyjen sinkkujen deitti</h1>
+      <Menu />
+      <Switch>
+        <Route path="/login">
+          <LoginForm />
+        </Route>
+        <Route path="/register">
+          <RegistrationForm />
+        </Route>
+      </Switch>
+    </div>
+  )
+}
 
 export default App
