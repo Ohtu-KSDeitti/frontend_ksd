88dc2949258796e2e65e73d126dd554054a89bca
StrappedGlint13 <matiasbrax@gmail.com>
2021-05-26 09:26:43 +0300

    registeration_form and login_form mods, a bit cleaning

--- src/App.js
+++ src/App.js
@@ -2,36 +2,38 @@ import React from 'react'
 import {
   Switch, Route,
 } from 'react-router-dom'
-import { useQuery, gql } from '@apollo/client'
+// import { useQuery } from '@apollo/client'
 import LoginForm from './components/LoginForm'
 import RegistrationForm from './components/RegistrationForm'
 import Menu from './components/Menu'
 
-const GET_PING = gql`
-  query {
-    pings {
-      ping
-    }
-  }
-`
+const testUsers = [
+  {
+    username: 'Matti Meikalainen',
+    password: 'kissakala123',
+  },
+  {
+    username: 'Ulla Uliuli',
+    password: 'kala123',
+  },
+]
 
-const App = () => {
-  const result = useQuery(GET_PING)
-  console.log(result)
-  return (
-    <div className="container">
-      <h1>Kristittyjen sinkkujen deitti</h1>
-      <Menu />
-      <Switch>
-        <Route path="/login">
-          <LoginForm />
-        </Route>
-        <Route path="/register">
-          <RegistrationForm />
-        </Route>
-      </Switch>
+const App = () => (
+  <div className="container">
+    <h1>Kristittyjen sinkkujen deitti</h1>
+    <Menu />
+    <Switch>
+      <Route path="/login">
+        <LoginForm testUsers={testUsers} />
+      </Route>
+      <Route path="/register">
+        <RegistrationForm />
+      </Route>
+    </Switch>
+    <div>
+      {testUsers.map((p) => p.username).join(', ')}
     </div>
-  )
-}
+  </div>
+)
 
 export default App
