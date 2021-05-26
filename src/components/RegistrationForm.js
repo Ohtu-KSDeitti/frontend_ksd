5c38e0be0f4b4284c0ac39571c665604df9958f2
StrappedGlint13 <matiasbrax@gmail.com>
2021-05-26 14:48:26 +0300

    more fields to registerform

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -7,16 +7,25 @@ const RegistrationForm = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConf, setPasswordConf] = useState('')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [gender, setGender] = useState('')
+  const [age, setAge] = useState('')
+
   const [createUser] = useMutation(CREATE_USER)
 
   const submit = async (event) => {
     event.preventDefault()
+    console.log(gender)
 
     createUser({ variables: { username, password, passwordConf } })
 
     setUsername('')
     setPassword('')
     setPasswordConf('')
+    setName('')
+    setEmail('')
+    setAge('')
   }
 
   return (
@@ -30,6 +39,12 @@ const RegistrationForm = () => {
             value={username}
             onChange={({ target }) => setUsername(target.value)}
           />
+          <Form.Label>Oma nimi:</Form.Label>
+          <Form.Control
+            type="text"
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
           <Form.Label>Salasana:</Form.Label>
           <Form.Control
             type="password"
@@ -42,6 +57,24 @@ const RegistrationForm = () => {
             value={passwordConf}
             onChange={({ target }) => setPasswordConf(target.value)}
           />
+          <Form.Label>Sähköposti:</Form.Label>
+          <Form.Control
+            type="text"
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+          />
+          <Form.Label>Ikä:</Form.Label>
+          <Form.Control
+            type="text"
+            value={age}
+            onChange={({ target }) => setAge(target.value)}
+          />
+          <Form.Label>Sukupuoli:</Form.Label>
+          <Form.Control as="select" onChange={({ target }) => setGender(target.value)}>
+            <option value="Mies">Mies</option>
+            <option value="Nainen">Nainen</option>
+            <option value="Muu">Muu</option>
+          </Form.Control>
           <Button type="submit">Rekisteröidy</Button>
         </Form.Group>
       </Form>
