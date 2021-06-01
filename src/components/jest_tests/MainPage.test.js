1feb052feae5120dc96c2b88d76bca57530a933d
StrappedGlint13 <matiasbrax@gmail.com>
2021-06-01 08:04:32 +0300

    lint fix

--- src/components/jest_tests/MainPage.test.js
+++ src/components/jest_tests/MainPage.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
-import MainPage from '../MainPage'
+import MainPage from '../templates/MainPage'
 
 test('renders content', () => {
   const component = render(
