172f12aa0a28f8729c17c1cf5c2ef1486d9b0a5d
StrappedGlint13 <matiasbrax@gmail.com>
2021-06-02 19:33:22 +0300

    small fix

--- src/components/utils/Footer.js
+++ src/components/utils/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { SocialIcon } from 'react-social-icons'
 
 const Footer = () => (
-  <footer className="bg-light text-center text-white" style={{ paddingTop: '20em' }}>
+  <footer className="content text-center text-white" style={{ paddingTop: '20em' }}>
     <div>
       <SocialIcon url="https://www.facebook.com/groups/575539649714586/discussion/preview" />
       <SocialIcon url="https://github.com/Ohtu-KSDeitti/frontend_ksd" />
