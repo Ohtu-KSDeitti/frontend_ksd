6dd5da017dcf14721829e469ad28add0a54d6dc9
Matias Aalto-Setälä <matias.aaltosetala@gmail.com>
2021-06-17 13:47:22 +0300

    toimiva navbargit add .

--- src/components/templates/Menu.js
+++ src/components/templates/Menu.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Nav, Navbar } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
 import '../../index.css'
 
 const Menu = ({ loggedUser, logout }) => {
@@ -10,67 +9,31 @@ const Menu = ({ loggedUser, logout }) => {
 
   if (!loggedUser) {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="dark">
+      <Navbar bg="light" variant="dark">
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="palkki">
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/">Pääsivu</Link>
-            </Nav.Link>
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/register">Rekisteröidy</Link>
-            </Nav.Link>
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/login">Kirjaudu sisään</Link>
-            </Nav.Link>
+            <a href="/" style={padding}>Pääsivu</a>
+            <a id="registerform" href="/register" style={padding}>Rekisteröidy</a>
+            <a id="loginform" href="/login" style={padding}>Kirjaudu sisään</a>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
     )
   }
   return (
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="dark">
+    <Navbar bg="light" variant="dark">
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="palkki">
-          <Nav.Link href="#" as="span">
-            <Link style={padding} to="/">Pääsivu</Link>
-          </Nav.Link>
-          <Nav.Link href="#" as="span" id="userpage">
-            <Link style={padding} to={`/${loggedUser}`}>Oma sivu</Link>
-          </Nav.Link>
-          <Nav.Link href="#" as="span" id="settings">
-            <Link style={padding} to={`/s/${loggedUser}`}>Asetukset</Link>
-          </Nav.Link>
-          <Nav.Link href="#" as="span" id="loginform">
-            <Link style={padding} to="/" onClick={logout}>Kirjaudu ulos</Link>
-          </Nav.Link>
+          <a href="/" style={padding}>Pääsivu</a>
+          <a id="userpage" href={`/${loggedUser}`} style={padding}>Oma sivu</a>
+          <a id="settings" href={`/s/${loggedUser}`} style={padding}>Asetukset</a>
+          <a id="loginform" href="/login" style={padding} onClick={logout}>Kirjaudu ulos</a>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   )
 }
-
-/*
-  if (!loggedUser) {
-    return (
-      <div>
-        <a href="/" style={padding}>Pääsivu</a>
-        <a id="registerform" href="/register" style={padding}>Rekisteröidy</a>
-        <a id="loginform" href="/login" style={padding}>Kirjaudu sisään</a>
-      </div>
-    )
-  }
-  return (
-    <div>
-      <a href="/" style={padding}>Pääsivu</a>
-      <a id="userpage" href={`/${loggedUser}`} style={padding}>Oma sivu</a>
-      <a id="settings" href={`/s/${loggedUser}`} style={padding}>Asetukset</a>
-      <a id="loginform" href="/login" style={padding} onClick={logout}>Kirjaudu ulos</a>
-    </div>
-
-  )
-}
-*/
 
 export default Menu
