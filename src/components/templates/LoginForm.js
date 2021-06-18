184345c6a13b254874c6d4c32c68774565b9fe22
Matias Aalto-Setälä <matias.aaltosetala@gmail.com>
2021-06-18 10:20:42 +0300

    ulkoasumuutokset branchiin

--- src/components/templates/LoginForm.js
+++ src/components/templates/LoginForm.js
@@ -54,7 +54,7 @@ const LoginForm = ({ setLoggedUser, setToken }) => {
       <Notification message={notification} />
       <Form onSubmit={submit}>
         <Form.Group>
-          <Form.Label>Sähköposti:</Form.Label>
+          <Form.Label><b>Sähköposti</b></Form.Label>
           <Form.Control
             id="email"
             required
@@ -62,7 +62,7 @@ const LoginForm = ({ setLoggedUser, setToken }) => {
             value={email}
             onChange={({ target }) => setEmail(target.value)}
           />
-          <Form.Label>Salasana:</Form.Label>
+          <Form.Label><b>Salasana</b></Form.Label>
           <Form.Control
             id="password"
             required
