e7478d9584aa4ac31cc0309f722e1b315865fa6f
Erkka Rahikainen <ergga96@gmail.com>
2021-06-23 10:22:25 +0300

    Fix script

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -2,6 +2,7 @@
 sudo yum update -y
 sudo yum install docker -y
 sudo service docker start
+sudo docker prune -f
 sudo docker kill $(docker ps -q)
 sudo cp /home/ec2-user/.env /home/ec2-user/frontend_ksd
 sudo docker build /home/ec2-user/frontend_ksd/ -t frontend_ksd
